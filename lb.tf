ad3fd6741c66d298f7786f849a54af32d74a3dd7
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-04 13:08:51 +0530

    updating tf-module-mutable-lb

--- lb.tf
+++ lb.tf
@@ -10,20 +10,6 @@ resource "aws_lb" "public" {
   }
 }
 
-//adding the frontend end instance target grp to public lb
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.public.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "arn:aws:acm:us-east-1:124374336606:certificate/534f69be-bcc8-444e-9eb3-973a2f0d10d3"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.target-grp.arn
-  }
-}
-
 
 resource "aws_lb" "private" {
   name               = "${local.TAG_NAME}-private-lb"
