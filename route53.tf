d54058f504a380e143ec20aefd0fcf4038ff094d
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-08 11:37:05 +0530

    updating tf-module-mutable-lb

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "replacing_lb_dns_new_dns" {
   zone_id = var.PUBLIC_HOSTED_ZONE_ID
-  name    = "app-$(var.ENV}.devops65.online"
+  name    = "app-${var.ENV}.devops65.online"
   type    = "CNAME"
   ttl     = 300
   records = [aws_lb.public.dns_name]
